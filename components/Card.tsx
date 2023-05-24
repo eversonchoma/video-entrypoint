f1768e666ed8d6e75462d3b00f88cae8cb29b72f
everson.choma <everson.choma@gmail.com>
2023-05-24 15:36:33 -0300

    feat: add new pages and animations

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,7 +12,7 @@ function Card({ href, imgSrc, layoutId }: CardProps) {
   return (
     <Link href={href}>
       <div className="relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition">
-        <motion.img src={imgSrc} className="w-full h-full" key={layoutId} />
+        <motion.img src={imgSrc} className="w-full h-full" layoutId={layoutId} />
       </div>
     </Link>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,29 @@
 import Card from "../components/Card";
-import Layout from "../components/Layout";
-import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <Layout>
+    <div>
       <h1 className="text-4xl sm:text-6xl kg:text-7xl mt-10 md:mt-24 leading-none font-extrabold trackng-tight text-gray-900">
         Let's build modern websites without leaving HTML behind
       </h1>
 
       <div className="mt-10 md:mt-24 grid w-full grid-cols-1 grid-rows-3 gap-4 md:grid-cols-3 md:grid-rows-1">
-        <Card href={"/nextjs"} imgSrc={"/img/nextjs.svg"} layoutId={"next-js-logo"} ></Card>
-        <Card href={"/tailwind"} imgSrc={"/img/tailwind.svg"} layoutId={"tailwind-logo"} ></Card>
-        <Card href={"/framermotion"} imgSrc={"/img/framermotion.svg"} layoutId={"framer-motion-logo"} ></Card>
+        <Card
+          href={"/nextjs"}
+          imgSrc={"/img/nextjs.svg"}
+          layoutId={"nextjs-logo"}
+        ></Card>
+        <Card
+          href={"/tailwind"}
+          imgSrc={"/img/tailwind.svg"}
+          layoutId={"tailwind-logo"}
+        ></Card>
+        <Card
+          href={"/framermotion"}
+          imgSrc={"/img/framermotion.svg"}
+          layoutId={"framer-motion-logo"}
+        ></Card>
       </div>
-    </Layout>
+    </div>
   );
 }
